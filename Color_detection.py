ff5f76f6c1abda960a047278497a67ec1c8ea7f2
user5Krymsk <user5Krymsk@gmail.com>
2021-10-27 16:38:45 +0300

    feat: добавили распознование цвета

--- Color_detection.py
+++ Color_detection.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
 
+frameWidht = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3,frameWidht)
+cap.set(4,frameHeight)
+
 def empty(a):
     pass
 
-img:np.ndarray = cv2.imread('robot.jpeg', cv2.IMREAD_COLOR)
-shrink_koeff = 2
-img_h, img_w, _ = img.shape
-img = cv2.resize(img, (int(img_w//shrink_koeff),int(img_h/shrink_koeff)),cv2.INTER_NEAREST)
+# img:np.ndarray = cv2.imread('robot.jpeg', cv2.IMREAD_COLOR)
+# shrink_koeff = 2
+# img_h, img_w, _ = img.shape
+# img = cv2.resize(img, (int(img_w//shrink_koeff),int(img_h/shrink_koeff)),cv2.INTER_NEAREST)
 
 cv2.namedWindow("TrackBars")
 cv2.resizeWindow("TrackBars",640,240)
@@ -19,6 +25,7 @@ cv2.createTrackbar("Val Min","TrackBars",0,255,empty)
 cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
 
 while True:
+    success,img = cap.read()
     imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
     h_max = cv2.getTrackbarPos("Hue Max","TrackBars")
